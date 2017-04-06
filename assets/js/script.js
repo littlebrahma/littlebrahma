ddd2de4b12a97445d2462343bd0a822e6f5ce991
rakshu <rakshitha.chandra@gmail.com>
2017-04-06 17:12:13 +0530

    changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -50,7 +50,6 @@ $('input[type="radio"]').click(function() {
 
 $(".job_detail").hide();
 $(".career__link").click(function() {
-    debugger;
     var job=$(this).data("id");
     $(".job").hide();
   
@@ -59,7 +58,6 @@ $(".career__link").click(function() {
 });
 
 $( ".career__img2").click(function() {
-    debugger;
     $(".job_detail").hide();
     var job=$(".job").length;
     for(var i=0;i<=job;i++){
